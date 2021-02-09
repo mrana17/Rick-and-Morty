3cc06af3c9723e137b51539f1678d1fc3b2372da
mrana17 <rm_a@msn.com>
2021-02-09 17:22:50 +0100

    all characters

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -18,6 +18,16 @@ export type APICharacter = {
     url: string;
     created: string;
   };
+
+  export type APICharacters = {
+    info: {
+      count: number;
+      pages: number;
+      next: string;
+      prev: string | null;
+    };
+    results: APICharacter[];
+  };
   
   export async function getCharacter(id: number) {
     const response = await fetch(
@@ -32,4 +42,17 @@ export type APICharacter = {
       origin: { name: result.origin.name },
     };
     return character;
+  }
+
+  export async function getCharacters() {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/`);
+    const result = (await response.json()) as APICharacters;
+    const characters = result.results.map((apiCharacter) => ({
+      imgSrc: apiCharacter.image,
+      name: apiCharacter.name,
+      status: apiCharacter.status,
+      species: apiCharacter.species,
+      origin: { name: apiCharacter.origin.name },
+    }));
+    return characters;
   }
