25eb8d5e3dd89102e34a14b5c179415fbe565d41
mrana17 <rm_a@msn.com>
2021-02-10 18:18:36 +0100

    added some new features

--- src/components/Card/card.ts
+++ src/components/Card/card.ts
@@ -1,28 +1,40 @@
+import { Character } from "../../utils/api";
 import { createElement } from "../../utils/createElement";
 
-export function createCard({ imgSrc, name, status, species, origin }) {
-  return createElement("div", {
+export function createCard({
+  imgSrc,
+  name,
+  status,
+  species,
+  origin,
+}: Character) {
+  return createElement("article", {
     className: "card",
     childs: [
       createElement("img", {
         className: "card__portrait",
         src: imgSrc,
       }),
-      createElement("h2", {
-        className: "card__name",
-        innerText: name,
-      }),
-      createElement("p", {
-        className: "card__status",
-        innerText: `${status === "Alive" ? "🤖" : "💀"} - ${status}`,
-      }),
-      createElement("p", {
-        className: "card__species",
-        innerText: species,
-      }),
-      createElement("p", {
-        className: "card__origin",
-        innerText: origin.name,
+      createElement("div", {
+        className: "card__info",
+        childs: [
+          createElement("h2", {
+            className: "card__name",
+            innerText: name,
+          }),
+          createElement("p", {
+            className: "card__status",
+            innerText: `${status === "Alive" ? "🤖" : "💀"} - ${status}`,
+          }),
+          createElement("p", {
+            className: "card__species",
+            innerText: species,
+          }),
+          createElement("p", {
+            className: "card__origin",
+            innerText: origin.name,
+          }),
+        ],
       }),
     ],
   });
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,78 +1,92 @@
 export type APICharacter = {
-    id: number;
+  id: number;
+  name: string;
+  status: "Alive" | "Dead" | "unknown";
+  species: string;
+  type: string;
+  gender: "Female" | "Male" | "Genderless" | "unknown";
+  origin: {
     name: string;
-    status: "Alive" | "Dead" | "unknown";
-    species: string;
-    type: string;
-    gender: "Female" | "Male" | "Genderless" | "unknown";
-    origin: {
-      name: string;
-      url: string;
-    };
-    location: {
-      name: string;
-      url: string;
-    };
-    image: string;
-    episode: string[];
     url: string;
-    created: string;
   };
-
-  export type APICharacters = {
-    info: {
-      count: number;
-      pages: number;
-      next: string;
-      prev: string | null;
-    };
-    results: APICharacter[];
-  };
-
-  export type Character = {
-    imgSrc: string;
+  location: {
     name: string;
-    status: "Alive" | "Dead" | "unknown";
-    species: string;
-    origin: {
-      name: string;
-    };
+    url: string;
   };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
 
-  function convertToCharacter(apiCharacter: APICharacter): Character {
-    return {
-      imgSrc: apiCharacter.image,
-      name: apiCharacter.name,
-      status: apiCharacter.status,
-      species: apiCharacter.species,
-      origin: { name: apiCharacter.origin.name },
-    };
+export type APICharacters = {
+  info: {
+    count: number;
+    pages: number;
+    next: string;
+    prev: string | null;
+  };
+  results: APICharacter[];
+};
+
+export type Character = {
+  imgSrc: string;
+  name: string;
+  status: "Alive" | "Dead" | "unknown";
+  species: string;
+  origin: {
+    name: string;
+  };
+};
+
+function convertToCharacter(apiCharacter: APICharacter): Character {
+  return {
+    imgSrc: apiCharacter.image,
+    name: apiCharacter.name,
+    status: apiCharacter.status,
+    species: apiCharacter.species,
+    origin: { name: apiCharacter.origin.name },
+  };
 }
-  
-  export async function getCharacter(id: number) {
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character/${id}`
-    );
-    const result = (await response.json()) as APICharacter;
-    const character = convertToCharacter(result);
-    // {
-    //   imgSrc: result.image,
-    //   name: result.name,
-    //   status: result.status,
-    //   species: result.species,
-    //   origin: { name: result.origin.name },
-    // };
-    return character;
-  }
 
-  export async function getCharacters() {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/`);
-    if (!response.ok) {
-      return[];
-    }
-    const result = (await response.json()) as APICharacters;
-    const characters = result.results.map((apiCharacter) => 
-      convertToCharacter(apiCharacter)
+export async function getCharacter(id: number) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${id}`
+  );
+  if (!response.ok) {
+    const result = await response.json();
+    return {
+      imgSrc: "",
+      name: result.error,
+      status: "Dead",
+      species: "Human",
+      origin: {
+        name: "Internet",
+      },
+    } as Character;
+  }
+  const result = (await response.json()) as APICharacter;
+  const character = convertToCharacter(result);
+  // {
+  //   imgSrc: result.image,
+  //   name: result.name,
+  //   status: result.status,
+  //   species: result.species,
+  //   origin: { name: result.origin.name },
+  // };
+  return character;
+}
+
+export async function getCharacters(name?: string) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${name ? `?name=${name}` : ""}`
+  );
+  if (!response.ok) {
+    return [];
+  }
+  const result = (await response.json()) as APICharacters;
+  const characters = result.results.map(
+    (apiCharacter) => convertToCharacter(apiCharacter)
     // ({
     // imgSrc: apiCharacter.image,
     // name: apiCharacter.name,
@@ -80,6 +94,6 @@ export type APICharacter = {
     // species: apiCharacter.species,
     //   origin: { name: apiCharacter.origin.name },
     // }));
-    );
-    return characters;
-  }
+  );
+  return characters;
+}
